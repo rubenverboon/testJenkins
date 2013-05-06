e49a98f94da77937fa84859c87ce148fe74ef30e
Ruben Verboon <ruben@rubenverboon.nl>
2013-05-06 11:22:06 +0200

    initial test commit

---
+++ src/main/java/main/Bouwen.java
@@ -0,0 +1,15 @@
+package main;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ruben
+ * Date: 17-6-13
+ * Time: 10:52
+ * To change this template use File | Settings | File Templates.
+ */
+public class Bouwen {
+
+    public static void schrijf() {
+        System.out.println("hello, world!");
+    }
+}
